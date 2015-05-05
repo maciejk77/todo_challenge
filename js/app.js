872483e2caaa75e234cfed7347e42adad375eca8
Maciej Kurek <maciejk77@gmail.com>
2015-05-05 01:07:58 +0100

    Listing all tasks, adding new, Bootstrap buttons

--- js/app.js
+++ js/app.js
@@ -1,10 +1,30 @@
 var toDoApp = angular.module('toDoApp', []);
 function ToDoController($scope) {
   $scope.tasks = [
-    { toDoItem: 'Cleaning' }, 
-    { toDoItem: 'Shopping' }, 
-    { toDoItem: 'Laundry' },
-    { toDoItem: 'Ironing' }
-  ]
+    { todo: 'Cleaning', completed: false }, 
+    { todo: 'Shopping', completed: false }, 
+    { todo: 'Laundry', completed: false },
+    { todo: 'Ironing', completed: false }
+  ];
+    
+  $scope.getToDoCount = function () {
+    return $scope.tasks.length;
+  };
+    
+    
+  $scope.addToDo = function () {
+    $scope.tasks.push({todo:$scope.formToDoText, completed:false});
+    $scope.formToDoText = '';
+  };
+    
+  // $scope.clearCompleted = function () {
+  //     $scope.tasks = _.filter($scope.tasks, function(todo){
+  //         return !todo.completed;
+  //     });
+  // };
+
 }
+
 toDoApp.controller('ToDoController', ToDoController);
+
+
